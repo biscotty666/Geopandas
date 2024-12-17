d2c3b8e6b226ab8bc67e3993c6f883f3b6234930
biscotty666 <biscotty666@gmail.com>
2024-12-17 07:08:14 -0700

    Initial Commit; Ch 6.1 Vector Format;

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShell = pkgs.mkShell {
+        name = "python-venv";
+        venvDir = "./.venv";
+        buildInputs = with pkgs; [
+          (python3.withPackages(ps: with ps; [
+            ipython
+            jupyter
+            widgetsnbextension
+            ipympl
+            jupyter-nbextensions-configurator
+            jedi-language-server
+            ipywidgets
+            mypy
+            pandas
+            numpy
+            geopandas
+            pyogrio
+            matplotlib
+            pyproj
+            scipy
+            shapely
+            xarray
+            rioxarray
+            rasterio
+          ]))
+        ];
+        postVenvCreation = ''
+          unset SOURCE_DATE_EPOCH
+          pip install -r requirements.txt
+        '';
+
+        shellHook = ''
+            export BROWSER=floorp
+            #jupyter lab
+        '';
+
+        postShellHook = ''
+          # allow pip to install wheels
+          unset SOURCE_DATE_EPOCH
+        '';
+      };
+    }
+  );
+}
